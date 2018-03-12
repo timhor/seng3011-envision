3bc8d1e740a96a646785196df518f5c99de9e67e
Michael Tran <michaelqtran01@gmail.com>
2018-03-12 14:56:39 +1100

    Fixed up calculations to be in line with API specs

--- backend/libs/compute.py
+++ backend/libs/compute.py
@@ -8,18 +8,18 @@ from datetime import datetime, timedelta
 def get_ts_daily_adjusted(instr):
     """Get data directly from Alpha Vantage"""
     r = requests.get('https://www.alphavantage.co/query', params={
-        'function': 'TIME_SERIES_DAILY_ADJUSTED',
+        'function': 'TIME_SERIES_DAILY',  # Don't need their adjusted after all
         'symbol': instr,
         'apikey': os.environ['ALPHA_VANTAGE_API'],
-        'outputsize': 'full',  # compact
+        'outputsize': 'full',  # Alternative: compact
     })
 
-    # Data comes with metadata which we are not interested in
+    # Data comes with metadata that we don't need
     data = r.json()['Time Series (Daily)']
 
-    df = pd.DataFrame(data)
-    df = df.T.apply(pd.to_numeric)  # Transpose dataset and convert strings to numbers
-    df.index = pd.to_datetime(df.index)  # Convert index to datetime
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df = df.apply(pd.to_numeric)
+    df.index = pd.to_datetime(df.index)
     return df
 
 
@@ -41,13 +41,17 @@ def working_data(instr, target, lower, upper):
 
     # Tighten to the range we want (and show non-trading days too)
     df = df.reindex(pd.date_range(lower_date_extreme, upper_date_extreme, freq='D')).fillna(method='bfill')
+    #     df = df[(df.index >= lower_date_extreme) & (df.index <= upper_date_extreme)]
 
     # Tag with relative dates
     df = df.apply(tag_relative_date, axis=1, args=(target_date, lower_date, upper_date))
 
-    # Calculate the refined data we want to work with
-    df['Return'] = df['5. adjusted close'].diff()
-    df['Return (%)'] = df['5. adjusted close'].pct_change()
+    #  Calculate the data we want
+    #     df['Return'] = df['5. adjusted close'].diff()
+    #     df['Return (%)'] = df['5. adjusted close'].pct_change()
+    df['Return'] = df['4. close'].diff()
+    df['Return (%)'] = df['4. close'].pct_change()
+
 
     return df
 
