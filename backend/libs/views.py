a1789cbb08ffab0a2c59c121f3ef56fa93660e6f
vintonypadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2018-03-19 21:04:28 +1100

    Add metadata output

--- backend/libs/views.py
+++ backend/libs/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from libs import compute
+import time
 
 app = Flask('envision-server-api')
 
@@ -40,25 +41,63 @@ def api():
     except ValueError:
         instr = [instr]
 
-
     returns = []
+    metadata = []
     for i in instr:
+        success = False
+        start_time = None
+        end_time = None
+        elapsed_time = None
+        error_message = ""
+
         try:
+            start_time = time.time()
+
             df = compute.working_data(i, date, lower, upper)
             df = compute.filter_df(df, var_list)
             df.index = df.index.format()
             data = df.to_json(orient='index')
+
+            end_time = time.time()
+
+            success = True
+            elapsed_time = '{:.2f}ms'.format(((end_time - start_time) * 1000))
+            start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(start_time))
+            end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(end_time))
+            # file_name = ?  
+
         except Exception as e:
             print(e)
-            data = "Error: " + str(e)
+            error_message = "Error: " + str(e)
+            data = error_message
+
+            # start_time is set before argument parsing
+            start_time = None 
+            
         returns.append({
             'InstrumentID': i,
             'Data': data
         })
+        metadata.append({
+            'team' : 'Envision',
+            'module' : 'Envision_API v1.0',
+            'parameters' : {
+                'instr': instr,
+                'date' : date,
+                'var_list' : var_list,
+                'lower' : lower,
+                'upper' : upper
+            },
+            'success' : success,
+            'start_time' : start_time,
+            'end_time' : end_time,
+            'elapsed_time' : elapsed_time,
+            'error_message' : error_message
+        })
 
 
     payload = {
-        'Metadata': None,  # TODO: Add in meta data
+        'Metadata': metadata,
         'CompanyReturns': returns
     }
 
