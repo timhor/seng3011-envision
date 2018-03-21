c3baf3b89e8f7d316f6825310287f0fdcf00bfaf
Tim Hor <timothy.hor@outlook.com>
2018-03-21 13:04:56 +1100

    Add horizontal scrollbar to output

--- backend/static/scripts/generator.js
+++ backend/static/scripts/generator.js
@@ -15,12 +15,12 @@ function getData(){
 
     $.getJSON(`api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`, function(data){
         var dataString = JSON.stringify(data, null, 3);
-        document.getElementById('queryResults').innerHTML = `<pre><code>` + syntaxHighlight(dataString) + `</code></pre>`;
+        document.getElementById('queryResults').innerHTML = syntaxHighlight(dataString);
         document.getElementById('loadingSpinner').hidden = true;
         document.getElementById('queryResults').hidden = false;
     })
 };
- 
+
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
