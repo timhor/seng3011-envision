4090c744a4423fc8ab6c14c9aeeaec07d250b5f7
vintonypadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2018-03-27 10:23:15 +1100

    Change use of var to let

--- backend/static/scripts/generator.js
+++ backend/static/scripts/generator.js
@@ -4,28 +4,28 @@ window.onload = function() {
 };
 
 function getData(){
-    var instrumentID = document.getElementById('instrument_id').value;
-    var dateOfInterest = document.getElementById('date_of_interest').value;
-    var lowerWindow = document.getElementById('lower_window').value;
-    var upperWindow = document.getElementById('upper_window').value;
+    let instrumentID = document.getElementById('instrument_id').value;
+    let dateOfInterest = document.getElementById('date_of_interest').value;
+    let lowerWindow = document.getElementById('lower_window').value;
+    let upperWindow = document.getElementById('upper_window').value;
 
-    var cmReturn = document.getElementById('cm_checkbox').checked;
-    var avReturn = document.getElementById('av_checkbox').checked;
-    var listOfVar = getListOfVars(cmReturn, avReturn);
+    let cmReturn = document.getElementById('cm_checkbox').checked;
+    let avReturn = document.getElementById('av_checkbox').checked;
+    let listOfVar = getListOfVars(cmReturn, avReturn);
 
     document.getElementById('queryResults').hidden = true;
     document.getElementById('loadingSpinner').hidden = false;
 
     if (cmReturn || avReturn) {
         $.getJSON(`api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`, (data) => {
-            var dataString = JSON.stringify(data, null, 3);
+            let dataString = JSON.stringify(data, null, 3);
             document.getElementById('queryResults').innerHTML = syntaxHighlight(dataString);
             document.getElementById('loadingSpinner').hidden = true;
             document.getElementById('queryResults').hidden = false;
         })
     } else {
         $.getJSON(`api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&lower_window=${lowerWindow}&upper_window=${upperWindow}`, (data) => {
-            var dataString = JSON.stringify(data, null, 3);
+            let dataString = JSON.stringify(data, null, 3);
             document.getElementById('queryResults').innerHTML = syntaxHighlight(dataString);
             document.getElementById('loadingSpinner').hidden = true;
             document.getElementById('queryResults').hidden = false;
@@ -36,7 +36,7 @@ function getData(){
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var colourClass = 'number';
+        let colourClass = 'number';
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
                 colourClass = 'key';
@@ -53,14 +53,14 @@ function syntaxHighlight(json) {
 }
 
 function updateAPIQuery(){
-    var instrumentID = document.getElementById('instrument_id').value;
-    var dateOfInterest = document.getElementById('date_of_interest').value;
-    var lowerWindow = document.getElementById('lower_window').value;
-    var upperWindow = document.getElementById('upper_window').value;
+    let instrumentID = document.getElementById('instrument_id').value;
+    let dateOfInterest = document.getElementById('date_of_interest').value;
+    let lowerWindow = document.getElementById('lower_window').value;
+    let upperWindow = document.getElementById('upper_window').value;
 
-    var cmReturn = document.getElementById('cm_checkbox').checked;
-    var avReturn = document.getElementById('av_checkbox').checked;
-    var listOfVar = getListOfVars(cmReturn, avReturn);
+    let cmReturn = document.getElementById('cm_checkbox').checked;
+    let avReturn = document.getElementById('av_checkbox').checked;
+    let listOfVar = getListOfVars(cmReturn, avReturn);
 
     if (cmReturn || avReturn) {
         var apiQuery = window.location.protocol + '//' + window.location.host + `/api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`;
@@ -71,22 +71,22 @@ function updateAPIQuery(){
 }
 
 function copyToClipboard() {
-    var copyText = document.getElementById("apiQuery");
+    let copyText = document.getElementById("apiQuery");
     copyText.select();
     document.execCommand("Copy");
 
-    var tooltip = document.getElementById("copyBtnTooltip");
+    let tooltip = document.getElementById("copyBtnTooltip");
     tooltip.innerHTML = "Copied!";
 }
 
 function copied() {
-    var tooltip = document.getElementById("copyBtnTooltip");
+    let tooltip = document.getElementById("copyBtnTooltip");
     tooltip.innerHTML = "Copy to Clipboard";
 }
 
 function getListOfVars(cmReturn, avReturn) {
     // TODO:  Change to underscores
-    var listOfVar = '';
+    let listOfVar = '';
     if (cmReturn && avReturn) {
         listOfVar = 'CM Return,AV Return';
     } else if (cmReturn) {
