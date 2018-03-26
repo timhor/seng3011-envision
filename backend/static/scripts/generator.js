31b1d5401a8402826449d0b0aa8d796689dc1bab
vintonypadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2018-03-26 22:19:18 +1100

    Change variables UI to checkbox and Fix Javascript accordingly

--- backend/static/scripts/generator.js
+++ backend/static/scripts/generator.js
@@ -6,19 +6,31 @@ window.onload = function() {
 function getData(){
     var instrumentID = document.getElementById('instrument_id').value;
     var dateOfInterest = document.getElementById('date_of_interest').value;
-    var listOfVar = document.getElementById('list_of_var').value;
     var lowerWindow = document.getElementById('lower_window').value;
     var upperWindow = document.getElementById('upper_window').value;
+
+    var cmReturn = document.getElementById('cm_checkbox').checked;
+    var avReturn = document.getElementById('av_checkbox').checked;
+    var listOfVar = getListOfVars(cmReturn, avReturn);
 
     document.getElementById('queryResults').hidden = true;
     document.getElementById('loadingSpinner').hidden = false;
 
-    $.getJSON(`api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`, function(data){
-        var dataString = JSON.stringify(data, null, 3);
-        document.getElementById('queryResults').innerHTML = syntaxHighlight(dataString);
-        document.getElementById('loadingSpinner').hidden = true;
-        document.getElementById('queryResults').hidden = false;
-    })
+    if (cmReturn || avReturn) {
+        $.getJSON(`api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`, function(data){
+            var dataString = JSON.stringify(data, null, 3);
+            document.getElementById('queryResults').innerHTML = syntaxHighlight(dataString);
+            document.getElementById('loadingSpinner').hidden = true;
+            document.getElementById('queryResults').hidden = false;
+        })
+    } else {
+        $.getJSON(`api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&lower_window=${lowerWindow}&upper_window=${upperWindow}`, function(data){
+            var dataString = JSON.stringify(data, null, 3);
+            document.getElementById('queryResults').innerHTML = syntaxHighlight(dataString);
+            document.getElementById('loadingSpinner').hidden = true;
+            document.getElementById('queryResults').hidden = false;
+        })
+    }
 };
 
 function syntaxHighlight(json) {
@@ -43,11 +55,18 @@ function syntaxHighlight(json) {
 function updateAPIQuery(){
     var instrumentID = document.getElementById('instrument_id').value;
     var dateOfInterest = document.getElementById('date_of_interest').value;
-    var listOfVar = document.getElementById('list_of_var').value;
     var lowerWindow = document.getElementById('lower_window').value;
     var upperWindow = document.getElementById('upper_window').value;
 
-    var apiQuery = window.location.protocol + '//' + window.location.host + `/api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`;
+    var cmReturn = document.getElementById('cm_checkbox').checked;
+    var avReturn = document.getElementById('av_checkbox').checked;
+    var listOfVar = getListOfVars(cmReturn, avReturn);
+
+    if (cmReturn || avReturn) {
+        var apiQuery = window.location.protocol + '//' + window.location.host + `/api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&list_of_var=${listOfVar}&lower_window=${lowerWindow}&upper_window=${upperWindow}`;
+    } else {
+        var apiQuery = window.location.protocol + '//' + window.location.host + `/api?instrument_id=${instrumentID}&date_of_interest=${dateOfInterest}&lower_window=${lowerWindow}&upper_window=${upperWindow}`;
+    }
     document.getElementById('apiQuery').innerText = apiQuery;
 }
 
@@ -63,4 +82,17 @@ function copyToClipboard() {
 function copied() {
     var tooltip = document.getElementById("copyBtnTooltip");
     tooltip.innerHTML = "Copy to Clipboard";
+}
+
+function getListOfVars(cmReturn, avReturn) {
+    // TODO:  Change to underscores
+    var listOfVar = '';
+    if (cmReturn && avReturn) {
+        listOfVar = 'CM Return,AV Return';
+    } else if (cmReturn) {
+        listOfVar = 'CM Return';
+    } else if (avReturn) {
+        listOfVar = 'AV Return';
+    }
+    return listOfVar;
 }
