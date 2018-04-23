3d04822ceef7ce74799129a4b5c073b8d2be3c48
vintonypadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2018-04-23 20:38:53 +1000

    Add sidebar navigation

--- frontend/src/app/app-material/app-material.module.ts
+++ frontend/src/app/app-material/app-material.module.ts
@@ -8,7 +8,8 @@ import {
   MatCardModule,
   MatSidenavModule,
   MatDividerModule,
-  MatInputModule
+  MatInputModule,
+  MatListModule
 } from '@angular/material';
 
 @NgModule({
@@ -20,7 +21,8 @@ import {
     MatCardModule,
     MatSidenavModule,
     MatDividerModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ],
   exports: [
     MatButtonModule,
@@ -30,7 +32,8 @@ import {
     MatCardModule,
     MatSidenavModule,
     MatDividerModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ]
 })
 export class AppMaterialModule { }
--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from '../app.component';
+import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: AppComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ]
 
