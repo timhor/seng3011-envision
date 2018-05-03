7980cdcc5d93eb19aa8d8085e4364ec053b9522b
vintonypadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2018-05-03 14:57:51 +1000

    Merge fail

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,19 +12,14 @@ import 'rxjs/add/observable/of';
 })
 export class AppComponent implements OnInit {
   public company = '';
-<<<<<<< HEAD
-  public stocksResponse: object;
-  public newsResponse: object;
-  private results: string[] = [];
-=======
-  public searchResponse: Object = null;
+  public stocksResponse: Object = null;
+  public newsResponse: Object = null;
   private companySuggestions: string[] = [];
   public startDate: Date = null;
   public endDate: Date = null;
   public news: Object = null;
 
   constructor(private callerService: CallerService) {}
->>>>>>> 25415d6b66621863186dd8b547c16c770329e350
 
   @ViewChild('sidenav') sidenav: MatSidenav;
   public navMode = 'side';
@@ -54,32 +49,6 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit() {
-<<<<<<< HEAD
-    let params: HttpParams = new HttpParams();
-    params = params.append('instrument_id', this.company);
-    params = params.append('date_of_interest', '2018-05-02');
-    this.callerService.getStockInfo(params).subscribe(
-      (result) => {
-        console.log(result);
-        this.stocksResponse = result;
-      }
-    );
-    params = new HttpParams();
-    params = params.append('company', this.company);
-    params = params.append('start_date', '2018-04-02');
-    params = params.append('end_date', '2018-05-02');
-    this.callerService.getNewsInfo(params).subscribe(
-      (result) => {
-        console.log(result);
-        this.newsResponse = result;
-      }
-    );
-  }
-
-  observableSource = (q: string): Observable<any[]> => {
-    this.results = q ? this.callerService.instrumentFuzzySearch(q) : [];
-    return (Observable.of(this.results));
-=======
     if (this.company.length > 0) {
       let stockParams: HttpParams = new HttpParams();
       stockParams = stockParams.append('instrument_id', this.company);
@@ -87,7 +56,7 @@ export class AppComponent implements OnInit {
       this.callerService.getStockInfo(stockParams).subscribe(
         (result) => {
           console.log(result);
-          this.searchResponse = result;
+          this.stocksResponse = result;
         }
       );
     }
@@ -100,14 +69,14 @@ export class AppComponent implements OnInit {
       this.callerService.getNewsInfo(newsParams).subscribe(
         (result) => {
           console.log(result);
-          this.news = result;
+          this.newsResponse = result;
         }
       );
     }
   }
 
-  updateDropdown(q: string) {
+  observableSource = (q: string): Observable<any[]> => {
     this.companySuggestions = q ? this.callerService.instrumentFuzzySearch(q) : [];
->>>>>>> 25415d6b66621863186dd8b547c16c770329e350
+    return (Observable.of(this.companySuggestions));
   }
 }
