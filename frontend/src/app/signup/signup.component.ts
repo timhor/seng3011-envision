f7350b849634960c6e0f4c0045c9d2314874c23a
vintonypadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2018-05-21 17:12:54 +1000

    Add Company list to SignUp

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from '../company';
+import { CallerService } from '../caller.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,20 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  public accountDetails: object = {
+  public accountDetails: any = {
     'username': '',
     'email': '',
     'password': '',
     'password_2': '',
     'company_list': []
   };
-  constructor() { }
+  public query: string;
+  private companySuggestions: Company[] = [];
+
+  constructor(private callerService: CallerService) {}
 
   ngOnInit() {
   }
 
   onSubmit() {
     console.log(this.accountDetails);
+  }
+
+  updateList() {
+    console.log(this.query);
+    if (!this.accountDetails.company_list.includes(this.query)) {
+      this.accountDetails.company_list.push(this.query);
+    }
+    console.log(this.accountDetails);
+  }
+
+  remove(index: number) {
+    this.accountDetails.company_list.splice(index, 1);
+  }
+
+  updateAutocomplete(q: string) {
+    q = q.replace(/\..*/, '');
+    q = q.replace(/.*\:/, '');
+    this.companySuggestions = q ? this.callerService.instrumentFuzzySearch(q) : [];
+    return this.companySuggestions;
   }
 
 }
