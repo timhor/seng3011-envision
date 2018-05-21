75789fd59c4e52a15e2b9c3f1002b7ecc0b5d929
Soham Patel <soham.patel@student.unsw.edu.au>
2018-05-21 20:33:39 +1000

    Display articles on Dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -12,6 +12,7 @@ import { Company } from '../company';
 })
 export class DashboardComponent implements OnInit {
   public query = 'CBA';
+  public userSubscriptions: any[] = [];
   public newsResponse: Object = null;
   public startDate: Date = null;
   public endDate: Date = null;
@@ -20,12 +21,16 @@ export class DashboardComponent implements OnInit {
   private pageSize = '10';
   public sortOptions: string[] = ['Relevance', 'Newest', 'Popularity'];
   public sortBy = 'Relevance';
+  private articlesPerCompany = 5;
 
   public constructor(private route: ActivatedRoute, private callerService: CallerService, private router: Router) {
     this.route.queryParams.subscribe(params => {
-      this.query = params['search_query'];
-      if (this.query) {
-        this.getQuery();
+
+      this.userSubscriptions = ['CBA', 'AMP'];
+      // GET A LIST OF COMPANIES THE USER IS SUBSCRIBED TO
+
+      if (this.userSubscriptions.length > 0) {
+        this.getNewsArticles();
       } else {
         this.newsResponse = null;
       }
@@ -36,68 +41,80 @@ export class DashboardComponent implements OnInit {
   }
 
 
-  private getQuery() {
+  private getNewsArticles() {
+    if (this.userSubscriptions.length > 5) {
+      this.articlesPerCompany = 2;
+    }
+
     this.searchedNews = [];
-    let newsParams: HttpParams = new HttpParams();
-    if (this.query.split(' ').length <= 2) {
-      newsParams = newsParams.append('q', '\"' + this.query + '\"');
-    } else {
-      newsParams = newsParams.append('q', '\"' + this.query.replace(/ (\w+)$/, '') + '\"');
-    }
 
-    let sortByVal = 'relevancy';
-    if (this.sortBy === this.sortOptions[1]) {
-      sortByVal = 'publishedAt';
-    } else if (this.sortBy === this.sortOptions[2]) {
-      sortByVal = 'popularity';
-    }
-    newsParams = newsParams.append('sortBy', sortByVal);
+    this.userSubscriptions.forEach(subscribedCompany => {
+      let articlesObtained = 0;
 
-    if (this.startDate !== null && this.endDate !== null) {
-      const from = new Date(this.startDate);
-      const to = new Date(this.endDate);
-      if (from.getTime() < to.getTime()) {
-        newsParams = newsParams.append('from', this.getDateString(from));
-        newsParams = newsParams.append('to', this.getDateString(to));
+      let newsParams: HttpParams = new HttpParams();
+      if (subscribedCompany.split(' ').length <= 2) {
+        newsParams = newsParams.append('q', '\"' + subscribedCompany + '\"');
+      } else {
+        newsParams = newsParams.append('q', '\"' + subscribedCompany.replace(/ (\w+)$/, '') + '\"');
       }
-    }
 
-    // Remove everything after '.' for CBA.AX cases otherwise, CBA will not be an option
-    this.query = this.query.replace(/\..*/, '');
-    // Remove everything before ':' for ASX:CBA cases
-    this.query = this.query.replace(/.*\:/, '');
-    this.query = this.query.toUpperCase();
-    const companies: Company[] = this.callerService.instrumentFuzzySearch(this.query);
-    let instrument = '';
-    companies.forEach(c => {
-      if (c.name.includes(this.query) || c.code.includes(this.query)) {
-        instrument = c.name;
+      let sortByVal = 'relevancy';
+      if (this.sortBy === this.sortOptions[1]) {
+        sortByVal = 'publishedAt';
+      } else if (this.sortBy === this.sortOptions[2]) {
+        sortByVal = 'popularity';
       }
-    });
-    this.callerService.getNewsInfo(newsParams).subscribe((result) => {
-      this.newsResponse = result;
-      this.newsResponse['articles'] = this.newsResponse['articles'].filter(e => {
-        // console.log(e);
-        if (e['title'].includes('&amp')) { return false; }
-        if (e['description'].includes('&amp')) { return false; }
-        if (e['source']['name'].includes('Salon.com')) { return false; }
-        return true;
+      newsParams = newsParams.append('sortBy', sortByVal);
+
+      if (this.startDate !== null && this.endDate !== null) {
+        const from = new Date(this.startDate);
+        const to = new Date(this.endDate);
+        if (from.getTime() < to.getTime()) {
+          newsParams = newsParams.append('from', this.getDateString(from));
+          newsParams = newsParams.append('to', this.getDateString(to));
+        }
+      }
+
+      // Remove everything after '.' for CBA.AX cases otherwise, CBA will not be an option
+      subscribedCompany = subscribedCompany.replace(/\..*/, '');
+      // Remove everything before ':' for ASX:CBA cases
+      subscribedCompany = subscribedCompany.replace(/.*\:/, '');
+      subscribedCompany = subscribedCompany.toUpperCase();
+      const companies: Company[] = this.callerService.instrumentFuzzySearch(subscribedCompany);
+      let instrument = '';
+      companies.forEach(c => {
+        if (c.name.includes(subscribedCompany) || c.code.includes(subscribedCompany)) {
+          instrument = c.name;
+        }
       });
-      this.newsResponse['articles'].forEach(e => {
-        const news = new NewsInfo('', '', '', '', '', '', instrument);
-        news.title = e['title'];
-        news.url = e['url'];
-        news.author = e['author'];
-        if (news.author === null) {
-          news.author = 'Unknown';
-        }
-        news.imageUrl = e['urlToImage'];
-        if (e['urlToImage'] === undefined || e['urlToImage'] === '' || e['urlToImage'] === null) {
-          news.imageUrl = 'http://via.placeholder.com/900x500';
-        }
-        news.description = e['description'];
-        news.date = new Date(e['publishedAt']).toLocaleDateString();
-        this.searchedNews.push(news);
+      this.callerService.getNewsInfo(newsParams).subscribe((result) => {
+        this.newsResponse = result;
+        this.newsResponse['articles'] = this.newsResponse['articles'].filter(e => {
+          // console.log(e);
+          if (e['title'].includes('&amp')) { return false; }
+          if (e['description'].includes('&amp')) { return false; }
+          if (e['source']['name'].includes('Salon.com')) { return false; }
+          return true;
+        });
+        this.newsResponse['articles'].forEach(e => {
+          if (articlesObtained !== this.articlesPerCompany) {
+            const news = new NewsInfo('', '', '', '', '', '', instrument);
+            news.title = e['title'];
+            news.url = e['url'];
+            news.author = e['author'];
+            if (news.author === null) {
+              news.author = 'Unknown';
+            }
+            news.imageUrl = e['urlToImage'];
+            if (e['urlToImage'] === undefined || e['urlToImage'] === '' || e['urlToImage'] === null) {
+              news.imageUrl = 'http://via.placeholder.com/900x500';
+            }
+            news.description = e['description'];
+            news.date = new Date(e['publishedAt']).toLocaleDateString();
+            this.searchedNews.push(news);
+            articlesObtained = 0;
+          }
+        });
       });
     });
   }
